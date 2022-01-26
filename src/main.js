3f9e3d7f36f26d9d1076b52a3b4ccea1ae6705a1
Sseve <719283952@qq.com>
2022-01-26 11:26:46 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// 导入element-ui
+// import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 按需导入element-ui
+import './plugins/element.js'
+
 
 Vue.config.productionTip = false
+// Vue.use(ElementUI)
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
