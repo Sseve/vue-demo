f353642c3bdd99c583b2f612834f934218ab97cf
Sseve <Sseve@github.com>
2022-02-16 16:41:09 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -8,8 +8,10 @@ import store from './store'
 import 'element-ui/lib/theme-chalk/index.css'
 // 按需导入element-ui
 import './plugins/element.js'
+// 引入axios
+import http from 'axios'
 
-
+Vue.prototype.$http = http
 Vue.config.productionTip = false
 // Vue.use(ElementUI)
 
